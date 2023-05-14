031d2839689d3984e6c8ea7c9e178f06aff151d0
singhh22 <singhh22@medtronic.com>
2023-05-14 11:25:34 +0530

    Display number from 1 to 6 when button is clicked.

--- app/src/main/java/com/example/rollthedice/MainActivity.kt
+++ app/src/main/java/com/example/rollthedice/MainActivity.kt
@@ -1,31 +1,30 @@
 package com.example.rollthedice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random.Default.nextInt
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
-        /*
-        Toast.makeText() is a method in Android that creates and displays a simple message on the
-        screen for a short duration. It is typically used to show a brief notification to the user,
-        such as a confirmation message or an error message.
-
-        The makeText() method is a static factory method of the Toast class, which means
-        we can call it without creating an instance of the Toast class.
-        It takes three parameters
-
-        The show() method is called at the end to actually show the message.
-
-        The context object allows to communicate with and get information about current state of
-        Android OS.
-         */
         rollButton.setOnClickListener {
-            Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    /**
+     * Displays the number from 1 to 6
+     */
+    private fun rollDice() {
+        // Obtaining the reference of text view
+        val resultText: TextView = findViewById(R.id.result_text)
+        // Obtaining a number from 1 to 6
+        val randomNumber = nextInt(6) + 1
+        // Setting the text of text view between 1 to 6
+        resultText.text = randomNumber.toString()
     }
 }
